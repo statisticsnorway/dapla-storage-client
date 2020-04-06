ca9fe7b51a50bfb946387012a7d553ed7d286606
Øyvind Strømmen <insula.ventus@gmail.com>
2020-04-06 21:28:23 +0200

    Sanitize unused file

--- src/main/java/no/ssb/dapla/storage/client/UnsupportedMediaTypeException.java
+++
@@ -1,7 +0,0 @@
-package no.ssb.dapla.storage.client;
-
-public class UnsupportedMediaTypeException extends Exception {
-    public UnsupportedMediaTypeException(String message) {
-        super(message);
-    }
-}
